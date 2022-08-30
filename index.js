d792dde4d169020f90364e5e66334135346fcfae
Marcus Lirio <90535919+MarcusLirio@users.noreply.github.com>
2022-08-29 23:39:31 -0300

    foto

---
+++ index.js
@@ -0,0 +1,9 @@
+<script src="https://code.jquery.com/jquery-3.6.1.js"></script>
+    var o = $(".card");
+    $(".top").on("mousemove", function (t) {
+        var e = -($(window).innerWidth() / 2 - t.pageX) / 30,
+            n = -($(window).innerHeight() / 2 - t.pageY) / 10;
+
+        o.attr("style", "transform: rotateY(" + e +"deg)rotateX("+ n +"deg);-webkit-transform: rotateY("+ e +"deg) rotateX("+ n +"deg); -moz-transform: rotateY(" + e +"deg) rotateX("+ n +"deg)")
+
+    })
